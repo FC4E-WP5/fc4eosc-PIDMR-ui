ecd770f1068a713872314daab7d5ea40997665b3
Themis Zamani <themiszamani@gmail.com>
2025-04-25 11:54:30 +0300

    PIDMR-245: Update css

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -7,10 +7,22 @@ const NotFound = () => {
       <div className="not-found-content">
         <h1>404 - Page Not Found</h1>
         <p>The page you are looking for does not exist or has been moved</p>
+        <img
+          src="https://faircore4eosc.eu/themes/faircore/images/graphic/char1.svg"
+          alt="404 Not Found"
+          className="not-found-lady"
+        />
+        <p>&nbsp;</p>
         <Link to={ROUTES.HOME} className="btn btn-primary btn-lg">
           Return to Homepage
         </Link>
       </div>
+
+      <img
+        src="https://faircore4eosc.eu/themes/faircore/images/graphic/cloud.svg"
+        alt="404 Not Found"
+        className="not-found-cloudSvg"
+      />
     </div>
   );
 };
