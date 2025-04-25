e55e5502ab02d2324c387306b336fb910951de0a
Themis Zamani <themiszamani@gmail.com>
2025-04-25 14:53:48 +0300

    added images , updated css

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,17 +1,15 @@
 import { Link } from "react-router-dom";
 import ROUTES from "../../server/endpoints/routes";
+import ladyImage from "../../assets/char1.svg";
+import cloudImage from "../../assets/cloud.svg";
 
 const NotFound = () => {
   return (
     <div className="not-found-container">
       <div className="not-found-content">
-        <h1>404 - Page Not Found</h1>
+        <h1>404 - Not Found</h1>
         <p>The page you are looking for does not exist or has been moved</p>
-        <img
-          src="https://faircore4eosc.eu/themes/faircore/images/graphic/char1.svg"
-          alt="404 Not Found"
-          className="not-found-lady"
-        />
+        <img src={ladyImage} alt="404 Not Found" className="not-found-lady" />
         <p>&nbsp;</p>
         <Link to={ROUTES.HOME} className="btn btn-primary btn-lg">
           Return to Homepage
@@ -19,9 +17,9 @@ const NotFound = () => {
       </div>
 
       <img
-        src="https://faircore4eosc.eu/themes/faircore/images/graphic/cloud.svg"
+        src={cloudImage}
         alt="404 Not Found"
-        className="not-found-cloudSvg"
+        className="not-found-cloudsvg"
       />
     </div>
   );
